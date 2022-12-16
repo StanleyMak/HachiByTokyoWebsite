52da2d0e744b958f1b82ed31ffd114ad8520222d
dayumstir <dempstertay123@gmail.com>
2022-12-17 00:01:07 +0800

    made navbar and first element of landing page

---
+++ pages/booknow.js
@@ -0,0 +1,9 @@
+export default function BookNow() {
+    return (
+    <div style="width:100%;height:800px;">
+        <iframe src="https://booking.moego.pet/go/?name=HachibyTokyo&book=0" width="100%" height="100%" frameBorder="0" title="Online booking" scrolling="no"/>
+    </div>
+    )
+}
+
+// doesnt work!!
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,45 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Hachi by tokyo</title>
+        <link rel="icon" href="/head_icon.png" />
       </Head>
 
+      <header>
+        <div className={styles.header}>
+          <Image src="/header/logo.png" alt="Hachi by tokyo logo" width={200} height={100} />
+          <nav className={styles.nav_bar}>
+            <Link href="/" legacyBehavior><a className={styles.nav_item}>Home</a></Link>
+            <Link href="/services" legacyBehavior><a className={styles.nav_item}>Services</a></Link>
+            <Link href="/blog" legacyBehavior><a className={styles.nav_item}>Blog</a></Link>
+            <Link href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0" legacyBehavior><a className={styles.nav_item}>Book Now</a></Link>
+          </nav>
+        </div>
+      </header>
+
+      <hr></hr>
+
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.box}>
+          
+          <div className={styles.content}>
+            <p style={{ margin: 0, marginBottom: 20 }}>Pet Grooming and Pet Care</p>
+            <Link href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0" legacyBehavior><a className={styles.button}>Book Now</a></Link>
+          </div>
+          <div className={styles.image_wrapper}>
+            <Image src="/home/dog1.png" alt="Cute dog" fill/>
+          </div>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        <span dangerouslySetInnerHTML={{ "__html": "&copy;" }} />2022, Hachi by tokyo
       </footer>
 
       <style jsx>{`
