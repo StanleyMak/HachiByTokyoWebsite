90802c19f6aeaee26bb64d3eca96193350980b53
dayumstir <dempstertay123@gmail.com>
2022-12-25 22:32:36 +0800

    initial services page commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,26 +10,53 @@ export default function Home() {
         <title>Hachi by tokyo</title>
         <link rel="icon" href="/head_icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <style> @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap'); </style>
+        <style>
+          {" "}
+          @import
+          url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');{" "}
+        </style>
       </Head>
 
       <header>
         <Link href="/">
-          <Image src="/header/logo.png" alt="Hachi by tokyo logo" width={200} height={100} />
+          <Image
+            src="/header/logo.png"
+            alt="Hachi by tokyo logo"
+            width={200}
+            height={100}
+          />
         </Link>
         <nav className={styles.nav_bar}>
-          <Link href="/" legacyBehavior><a className={styles.nav_item}>Home</a></Link>
-          <Link href="/services" legacyBehavior><a className={styles.nav_item}>Services</a></Link>
-          <Link href="/blog" legacyBehavior><a className={styles.nav_item}>Blog</a></Link>
-          <Link href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0" legacyBehavior><a className={styles.nav_item}>Book Now</a></Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.nav_item}>Home</a>
+          </Link>
+          <Link href="/services" legacyBehavior>
+            <a className={styles.nav_item}>Services</a>
+          </Link>
+          <Link href="/blog" legacyBehavior>
+            <a className={styles.nav_item}>Blog</a>
+          </Link>
+          <Link
+            href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0"
+            legacyBehavior
+          >
+            <a className={styles.nav_item}>Book Now</a>
+          </Link>
         </nav>
       </header>
 
       <main>
         <section className={styles.box}>
           <div className={styles.content}>
-            <p style={{ margin: 0, marginBottom: 20 }}>Pet Grooming and Pet Care</p>
-            <Link href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0" legacyBehavior><a>Book Now</a></Link>
+            <p style={{ margin: 0, marginBottom: 20 }}>
+              Pet Grooming and Pet Care
+            </p>
+            <Link
+              href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0"
+              legacyBehavior
+            >
+              <a>Book Now</a>
+            </Link>
           </div>
           <div className={styles.image_wrapper}>
             <Image src="/home/dog1.png" alt="Cute dog" fill />
@@ -39,7 +66,12 @@ export default function Home() {
 
       <footer>
         <div className={styles.top}>
-          <Image src="/footer/logo.png" alt="Hachi by tokyo logo" width={160} height={80} />
+          <Image
+            src="/footer/logo.png"
+            alt="Hachi by tokyo logo"
+            width={160}
+            height={80}
+          />
           <div className={styles.contact}>
             <h1>Contact Us</h1>
             <p>21 Ghim Moh Road, #01-201, S 270021</p>
@@ -47,7 +79,12 @@ export default function Home() {
             <p>Call: 88696367</p>
             <p>Email Us: hachi@hachibytokyo.com</p>
           </div>
-          <Link href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0" legacyBehavior><a>Book Now</a></Link>
+          <Link
+            href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0"
+            legacyBehavior
+          >
+            <a>Book Now</a>
+          </Link>
         </div>
         <div className={styles.btm}>
           <div className={styles.policy}>
@@ -62,19 +99,32 @@ export default function Home() {
           </div>
           <div className={styles.socials}>
             <Link href="https://www.facebook.com/hachibytokyo/">
-              <Image src="/footer/fb_logo.png" alt="Hachi by tokyo logo" width={40} height={40} />
+              <Image
+                src="/footer/fb_logo.png"
+                alt="Hachi by tokyo logo"
+                width={40}
+                height={40}
+              />
             </Link>
             <Link href="https://www.instagram.com/hachibytokyo/?hl=en">
-              <Image src="/footer/ig_logo.png" alt="Hachi by tokyo logo" width={40} height={40} />
+              <Image
+                src="/footer/ig_logo.png"
+                alt="Hachi by tokyo logo"
+                width={40}
+                height={40}
+              />
             </Link>
           </div>
         </div>
         <div className={styles.copyright}>
           <Link href="/" legacyBehavior>
-            <a style={{ textDecoration: "none", color: "grey" }}><span dangerouslySetInnerHTML={{ "__html": "&copy;" }} />2022,&nbsp;Hachi by tokyo</a>
+            <a style={{ textDecoration: "none", color: "grey" }}>
+              <span dangerouslySetInnerHTML={{ __html: "&copy;" }} />
+              2022,&nbsp;Hachi by tokyo
+            </a>
           </Link>
         </div>
-      </footer >
+      </footer>
 
       <style jsx>{`
         main {
@@ -91,7 +141,7 @@ export default function Home() {
           align-items: flex-end;
           padding-top: 3rem;
           padding-bottom: 1rem;
-          border-bottom: 2px solid #F0B150;
+          border-bottom: 2px solid #f0b150;
         }
         footer {
           width: 100%;
@@ -100,7 +150,7 @@ export default function Home() {
           flex-direction: column;
           justify-content: flex-end;
           align-items: center;
-          background-color: #FFF6E8;
+          background-color: #fff6e8;
         }
         code {
           background: #fafafa;
@@ -124,6 +174,6 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div >
-  )
+    </div>
+  );
 }
--- pages/services.js
+++ pages/services.js
@@ -1,9 +1,102 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Services.module.css";
 
 export default function Services() {
   return (
-    <h1>Services</h1>
-  )
+    <div className={styles.container}>
+      <Head>
+        <title>Hachi by tokyo</title>
+        <link rel="icon" href="/head_icon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <style>
+          {" "}
+          @import
+          url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');{" "}
+        </style>
+      </Head>
+
+      <header>
+        <Link href="/">
+          <Image
+            src="/header/logo.png"
+            alt="Hachi by tokyo logo"
+            width={200}
+            height={100}
+          />
+        </Link>
+        <nav className={styles.nav_bar}>
+          <Link href="/" legacyBehavior>
+            <a className={styles.nav_item}>Home</a>
+          </Link>
+          <Link href="/services" legacyBehavior>
+            <a className={styles.nav_item}>Services</a>
+          </Link>
+          <Link href="/blog" legacyBehavior>
+            <a className={styles.nav_item}>Blog</a>
+          </Link>
+          <Link
+            href="https://booking.moego.pet/go/?name=HachibyTokyo&book=0"
+            legacyBehavior
+          >
+            <a className={styles.nav_item}>Book Now</a>
+          </Link>
+        </nav>
+      </header>
+
+      <main>
+
+      </main>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        header {
+          display: flex;
+          justify-content: space-evenly;
+          align-items: flex-end;
+          padding-top: 3rem;
+          padding-bottom: 1rem;
+          border-bottom: 2px solid #f0b150;
+        }
+        footer {
+          width: 100%;
+          height: 500px;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-end;
+          align-items: center;
+          background-color: #fff6e8;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: Roboto, sans-serif;
+          font-weight: 100;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
 }
