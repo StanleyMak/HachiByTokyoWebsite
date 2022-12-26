dd2dd1f091df9dfcba35665238602fa24a4edf08
dayumstir <dempstertay123@gmail.com>
2022-12-27 01:33:29 +0800

    temp code for booknow

--- pages/booknow.js
+++ pages/booknow.js
@@ -1,9 +1,5 @@
 export default function BookNow() {
     return (
-    <div style="width:100%;height:800px;">
-        <iframe src="https://booking.moego.pet/go/?name=HachibyTokyo&book=0" width="100%" height="100%" frameBorder="0" title="Online booking" scrolling="no"/>
-    </div>
+    <h1>BookNow</h1>
     )
 }
-
-// doesnt work!!
